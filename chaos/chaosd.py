29dd5ebb406ace70e7bc4b9062f283d573bf2c8a
Ayush <itsayushdwivedi@gmail.com>
2018-11-27 23:53:39 +0530

    Change class structure of the daemon

--- chaos/chaosd.py
+++ chaos/chaosd.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import functools
 import os
 import signal
 import sys
 
 from config import DaemonConfig
-from utils import randomhit
+from random import random
 
 from bs4 import BeautifulSoup, NavigableString
 from PyQt5.Qt import QGuiApplication, QClipboard
@@ -16,12 +17,25 @@ from PyQt5.QtCore import QObject, QMimeData
 """
 Add/Fix:
 1. Config options
-2. Preserve data after quitting
+2. Preserve data after (force) quitting
 3. Custom chars support
-4. Tests
-5. Coverage
+4. Tests and coverage
+5. Dependency Injection
 6. Daemonize
 """
+
+
+def enable_rhc(flag):
+    def decorator(func):
+        @functools.wraps(func)
+        def decorated_func(*args, **kwargs):
+            if random() < 0.5:
+                func(*args, **kwargs)
+        if flag:
+            return decorated_func
+        else:
+            return func
+    return decorator
 
 
 class ModifyData:
@@ -36,87 +50,93 @@ class ModifyData:
         return s
 
 
-class ClipboardHandler(QObject):
+class MimeHandler:
+
+    def __init__(self, mime_data, fin_mime_data, format_list):
+        self.mime_data = mime_data
+        self.fin_mime_data = fin_mime_data
+        self.format_list = format_list
+
+    def filter(self):
+        if not self.format_list:
+            return True
+
+        # Filter the MIME types "image/*" to ignore image data
+        # and "text/uri-list" to ignore files and directories.
+        if self.mime_data.hasImage() or self.mime_data.hasUrls():
+            return True
+
+        # Custom MIME data can vary wildly across applications, and
+        # usually takes the following format:
+        # application/x-y-z;value="somevalue"
+        # Currently, chaos does not modify objects with such MIME data.
+        for format in self.format_list:
+            if format.endswith('"'):
+                return True
+
+    def tset(self):
+        if self.mime_data.hasText():
+            plaintext = self.mime_data.text()
+            p = ModifyData(plaintext)
+            modified_plaintext = p.replace_chars()
+            self.fin_mime_data.setText(modified_plaintext)
+            self.format_list.remove('text/plain')
+
+    def hset(self):
+        if self.mime_data.hasHtml():
+            markup = self.mime_data.html()
+            soup = BeautifulSoup(markup, "html.parser")
+            for inner_text in list(soup.strings):
+                p = ModifyData(inner_text)
+                inner_text.replace_with(NavigableString(p.replace_chars()))
+            self.fin_mime_data.setHtml(str(soup))
+            self.format_list.remove('text/html')
+
+    def oset(self):
+        for format in self.format_list:
+            orig_data = self.mime_data.data(format)
+            self.fin_mime_data.setData(format, orig_data)
+
+
+class Clipboard(QObject):
 
     dc = DaemonConfig()
     dc.set_config()
     if dc.valid_config():
-        cfg = dc.custom_config
+        config = dc.custom_config
     else:
         sys.exit("Error: Daemon configuration file is invalid!")
 
     def __init__(self):
         super().__init__()
         clipboard = QGuiApplication.clipboard()
-        #clipboard.selectionChanged.connect(self.foo)
         clipboard.dataChanged.connect(self.overwrite)
 
-    def unsafe(self, mime_data):
-        format_list = mime_data.formats()
-        if not format_list:
-            return True
-
-        # Filter the MIME types "image/*" to ignore image data
-        # and "text/uri-list" to ignore files and directories.
-        if mime_data.hasImage() or mime_data.hasUrls():
-            return True
-
-        # Custom MIME data is highly specific and varies wildly across
-        # applications. It usually takes the format:
-        # application/x-y-z;value="somevalue"
-        # Currently, chaos does not modify objects with such MIME data.
-        for format in format_list:
-            if format.endswith('"'):
-                return True
-
-    def tset(self, mime_data, fin_mime_data, format_list):
-        if mime_data.hasText():
-            plaintext = mime_data.text()
-            p = ModifyData(plaintext)
-            modified_plaintext = p.replace_chars()
-            fin_mime_data.setText(modified_plaintext)
-            format_list.remove('text/plain')
-
-    def hset(self, mime_data, fin_mime_data, format_list):
-        if mime_data.hasHtml():
-            markup = mime_data.html()
-            soup = BeautifulSoup(markup, "html.parser")
-            for inner_text in list(soup.strings):
-                p = ModifyData(inner_text)
-                inner_text.replace_with(NavigableString(p.replace_chars()))
-            fin_mime_data.setHtml(str(soup))
-            format_list.remove('text/html')
-
-    def oset(self, mime_data, fin_mime_data, format_list):
-        for format in format_list:
-            orig_data = mime_data.data(format)
-            fin_mime_data.setData(format, orig_data)
-
-    def set_clipboard(self, fin_mime_data):
+    def set_mime_data(self, fin_mime_data):
         clipboard = QGuiApplication.clipboard()
         tmp = clipboard.blockSignals(True)
-        clipboard.setMimeData(fin_mime_data)
+        clipboard.setMimeData(fin_mime_data, mode = 0)
         clipboard.blockSignals(tmp)
 
-    @randomhit(cfg["random_hit_chance"])
+    @enable_rhc(config["random_hit_chance"])
     def overwrite(self):
         clipboard = QGuiApplication.clipboard()
-
-        mime_data = clipboard.mimeData()
+        mime_data = clipboard.mimeData(mode = 0)
         fin_mime_data = QMimeData()
         format_list = mime_data.formats()
+        mh = MimeHandler(mime_data, fin_mime_data, format_list)
 
-        if not self.unsafe(mime_data):
-            if not ClipboardHandler.cfg["plaintext_only"]:
-                self.tset(mime_data, fin_mime_data, format_list)
-                self.hset(mime_data, fin_mime_data, format_list)
-                self.oset(mime_data, fin_mime_data, format_list)
-                self.set_clipboard(fin_mime_data)
+        if not mh.filter():
+            if not Clipboard.config["plaintext_only"]:
+                mh.tset()
+                mh.hset()
+                mh.oset()
+                self.set_mime_data(fin_mime_data)
             else:
                 if not mime_data.hasHtml():
-                    self.tset(mime_data, fin_mime_data, format_list)
-                    self.oset(mime_data, fin_mime_data, format_list)
-                    self.set_clipboard(fin_mime_data)
+                    mh.tset()
+                    mh.oset()
+                    self.set_mime_data(fin_mime_data)
 
 
 if __name__ == "__main__":
@@ -138,5 +158,5 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     app = QGuiApplication(sys.argv)
-    c = ClipboardHandler()
+    c = Clipboard()
     sys.exit(app.exec_())
