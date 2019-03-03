cc92d5b16863c344b064de546746a03af360f179
Ayush <itsayushdwivedi@gmail.com>
2019-03-03 22:08:43 +0530

    Improve the robustness of build script

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Abort script at first error, when a command exits with non-zero status
+set -e
+
+# Set working directory to location of the script to prevent disasters
+cd $(dirname $0)
+basedir=$(pwd)
+
 # Create and activate a virtual Python environment
 virtualenv -p python3 chaos-build-env
 source chaos-build-env/bin/activate
@@ -21,13 +28,22 @@ pyinstaller --noconfirm \
 deactivate
 
 # Rearrangement and cleanup of files and directories
+cleanup () {
+    rm -rf chaos \
+        chaos-build-env \
+        chaos.egg-info \
+        pip-wheel-metadata
+    mv chaosd chaos
+    find . -maxdepth 1 -type f ! -name 'build.sh' -exec rm -f {} +
+    rm build.sh
+}
+
 cd dist
 mv chaosd ../..
 cd ../..
-rm -rf chaos \
-    chaos-build-env \
-    chaos.egg-info \
-    pip-wheel-metadata
-mv chaosd chaos
-find . -maxdepth 1 -type f ! -name 'build.sh' -exec rm -f {} +
-rm build.sh
+
+if [[ $(pwd) -ef "$basedir" ]]; then
+    cleanup
+else
+    exit 1
+fi
